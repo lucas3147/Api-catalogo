0d071a6d581283c9e4dee4f9b29eb834612eb355
Lucas Lima <lucas.souza3146@gmail.com>
2023-05-06 23:47:49 -0300

    criado pacotes e aplicações de migrations

---
+++ APICatalogo/APICatalogo/Migrations/20230507022033_populaProdutos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class populaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+               "Values('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg',50,GetDate(),7)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,GetDate(),8)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+               "Values('Pudim 100 g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,GetDate(),9)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
